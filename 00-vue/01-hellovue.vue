adeb7d0c59581596ada456fa9c1c4af3ba1d389d
zhengxiangxia <nazi_hestia@163.com>
2018-04-18 15:11:36 +0800

    vuejs学习

---
+++ 00-vue/01-hellovue.vue
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Vue第一个页面</title>
+	<script src="https://cdn.bootcss.com/vue/2.4.2/vue.min.js"></script>
+</head>
+<body>
+	<div id="vue_det">
+		<h1>site:{{site}}</h1>
+		<h1>author:{{author}}</h1>
+		<h1>mobile:{{mobile()}}</h1>
+	</div>
+	<script type="text/javascript">
+		var vm=new Vue({
+			el:'#vue_det',
+			data:{
+				site:"第一个vue页面",
+				author:"zxx"
+			},
+			methods:{
+				mobile:function(){
+					return "18613131313";
+				}
+			}
+		})
+		document.write(vm.a===data.a)
+	</script>
+</body>
+</html>
---
+++ 00-vue/02-vhtml.vue
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>vhtml</title>
+	<script type="text/javascript" src="https://cdn.bootcss.com/vue/2.4.2/vue.min.js"></script>
+</head>
+<body>
+	<div id="app">
+		<div v-html="message"></div>
+	</div>
+
+	<script type="text/javascript">
+		new Vue({
+			el:'#app',
+			data:{
+				message:'<h1>zxx小侠</h1>'
+			}
+		})
+	</script>
+</body>
+</html>
